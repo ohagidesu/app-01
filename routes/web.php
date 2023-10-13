77aca6f5310cfac2ac2d20af426b75e448ab3441
ohagidesu <bb15121076@ms.nagasaki-u.ac.jp>
2023-10-13 15:38:12 +0900

    成果物のマルチログイン機能

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Controllers\Admin\AdminRegisterController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -35,3 +38,29 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/chat', [ChatController::class, 'index']);
 Route::post('/chat', [ChatController::class, 'sendMessage']);
+
+/*
+|--------------------------------------------------------------------------
+| 管理者用ルーティング
+|--------------------------------------------------------------------------
+*/
+Route::group(['prefix' => 'admin'], function () {
+    // 登録
+    Route::get('register', [AdminRegisterController::class, 'create'])
+        ->name('admin.register');
+
+    Route::post('register', [AdminRegisterController::class, 'store']);
+
+    // ログイン
+    Route::get('login', [AdminLoginController::class, 'showLoginPage'])
+        ->name('admin.login');
+
+    Route::post('login', [AdminLoginController::class, 'login']);
+
+    // 以下の中は認証必須のエンドポイントとなる
+    Route::middleware(['auth:admin'])->group(function () {
+        // ダッシュボード
+        Route::get('dashboard', fn() => view('admin.dashboard'))
+            ->name('admin.dashboard');
+    });
+});
